753079afb690b23367374b8702d972d41bdf3abc
Adam Herst <adamherst@adamherst.com>
2020-10-20 15:57:31 -0400

    Add properties and methods to the canvas and composition objects and start spaces.

--- sketch.js
+++ sketch.js
@@ -4,14 +4,32 @@
 let canvas = Object();
 
 function setup() {
-  canvas = createCanvas(windowWidth/2,windowHeight/2);
-  canvas.background('grey');
 
-  console.log("composition" in canvas);
-  canvas.composition = new Composition();
-  if ("composition" in canvas)
-  console.log(canvas.composition.name);
+  canvas = createCanvas(windowWidth/2,windowHeight/2);
+
+  // add properties and methods to the canvas object
+  if (!("center" in canvas)) canvas.center = function ()  {
+    canvas.position((windowWidth - width) / 2, (windowHeight - height) / 2);
+  }
+  canvas.center();
+
+  if (!("centerOrigin" in canvas)) canvas.centerOrigin = function ()  {
+    translate((windowWidth - width) / 2, (windowHeight - height) / 2);
+  }
+
+  if (!("composition" in canvas)) canvas.composition = new Composition();
+  canvas.composition.width = canvas.width/2;
+  canvas.composition.height = canvas.height/2;
+
+  // setup p5.js modes
+  rectMode(CENTER);
+
 }
 
 function draw() {
+  canvas.background('red');
+  canvas.centerOrigin();
+  rect(0,0,canvas.composition.width,canvas.composition.height);
+
+  noLoop();
 }
