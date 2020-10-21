f77fe4e32d1a3d11ff847584d984fa5514ace4f5
Adam Herst <adamherst@adamherst.com>
2020-10-21 12:20:26 -0400

    Start work on choosing the centre for the space.

--- Space.js
+++ Space.js
@@ -1,3 +1,36 @@
 function Space() {
-  this.message = "hi from Space.js";
+  this.center = {
+    x: 0,
+    y: 0,
+  };
+  this.radius = 0;
+
+  this.center = pickCenter();
+  if (moreSpaces == true) {
+    this.radius = pickRadius(this.center);
+  }
+
+  function pickCenter() {
+    let numTries = 0;
+    let center = pickPoint();
+
+    // does this point lie in another space in the composition?
+    for (let i = 0; i < spaces.length; i++) {
+      while (dist(center.x, center.y, spaces[i].center.x, spaces[i].center.y) <= spaces[i].radius && numTries < maxTries) {
+        numTries++;
+        (numTries >= maxTries) ? moreSpaces = false : center = pickPoint();
+        i = 0;
+      }
+    }
+    return(center);
+  }
+
+  function pickPoint() {
+    return {
+      x: int(random(-mat.width/2, mat.width/2)),
+      y: int(random(-mat.height/2, mat.height/2)),
+    };
+  }
+
+
 }
--- sketch.js
+++ sketch.js
@@ -1,25 +1,22 @@
 "use strict"
 
-// is it possible to have only one global variable for the global object?
 let canvas = Object();
 
 function setup() {
-
   canvas = createCanvas(windowWidth/2,windowHeight/2);
 
   // add properties and methods to the canvas object
   if (!("center" in canvas)) canvas.center = function ()  {
     canvas.position((windowWidth - width) / 2, (windowHeight - height) / 2);
   }
-  canvas.center();
 
   if (!("centerOrigin" in canvas)) canvas.centerOrigin = function ()  {
     translate((windowWidth - width) / 2, (windowHeight - height) / 2);
   }
-
   if (!("composition" in canvas)) canvas.composition = new Composition();
 
-  // setup p5.js modes
+  canvas.center();
+
   rectMode(CENTER);
 
 }
