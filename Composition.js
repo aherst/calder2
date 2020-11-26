bac7f13cf85fae4f1a65224def6996c37032a30d
Adam Herst <adamherst@adamherst.com>
2020-11-26 11:31:41 -0500

    Add mat border for printing and framing.

--- Composition.js
+++ Composition.js
@@ -1,7 +1,14 @@
 function Composition() {
   this.name = "calder2_" + year() + month() + day() + hour() + minute() + second();
-  this.width = canvas.width;
-  this.height = canvas.height;
+
+  this.paperWidth = 8.5;
+  this.paperHeight = 11;
+  this.mattedWidth = 7.5;
+  this.mattedHeight = 9.5;
+  
+  this.width = canvas.width * this.mattedWidth/this.paperWidth;
+  this.height = canvas.height * this.mattedHeight/this.paperHeight;
+
   this.spaces = [];
 
 }
